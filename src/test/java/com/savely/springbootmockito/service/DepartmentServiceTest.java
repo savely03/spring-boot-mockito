ed100ded1192ce8b60b98c363827a1fa74b5997d
Savely Domnikov <savely.domnikov@yandex.ru>
2023-05-16 02:05:57 +0300

    Created BeforeEach method

--- src/test/java/com/savely/springbootmockito/service/DepartmentServiceTest.java
+++ src/test/java/com/savely/springbootmockito/service/DepartmentServiceTest.java
@@ -3,13 +3,13 @@ package com.savely.springbootmockito.service;
 import com.savely.springbootmockito.exception.IncorrectDepartmentException;
 import com.savely.springbootmockito.model.Employee;
 import com.savely.springbootmockito.service.impl.DepartmentServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -30,6 +30,11 @@ class DepartmentServiceTest {
 
     private final List<Employee> employees = List.of(EMPLOYEE_1_DEP_1, EMPLOYEE_2_DEP_1, EMPLOYEE_1_DEP_2, EMPLOYEE_2_DEP_2);
 
+    @BeforeEach
+    void setUp() {
+        lenient().when(employeeService.findAllEmployees()).thenReturn(employees);
+    }
+
     @Test
     void checkInvalidDepartmentTest() {
         assertThatExceptionOfType(IncorrectDepartmentException.class).isThrownBy(
@@ -39,8 +44,6 @@ class DepartmentServiceTest {
 
     @Test
     void findEmployeesByFirstDepartmentTest() {
-        when(employeeService.findAllEmployees()).thenReturn(employees);
-
         List<Employee> employees = out.findEmployeesByDepartment(FIRST_DEP);
 
         assertThat(employees).hasSize(2).containsOnly(EMPLOYEE_1_DEP_1, EMPLOYEE_2_DEP_1);
@@ -48,8 +51,6 @@ class DepartmentServiceTest {
 
     @Test
     void findEmployeesBySecondDepartmentTest() {
-        when(employeeService.findAllEmployees()).thenReturn(employees);
-
         List<Employee> employees = out.findEmployeesByDepartment(SECOND_DEP);
 
         assertThat(employees).hasSize(2).containsOnly(EMPLOYEE_1_DEP_2, EMPLOYEE_2_DEP_2);
@@ -57,58 +58,36 @@ class DepartmentServiceTest {
 
     @Test
     void calculateSumSalariesByFirstDepartmentTest() {
-        when(employeeService.findAllEmployees()).thenReturn(employees);
-
         assertThat(out.calculateSumSalariesByDepartment(FIRST_DEP)).isEqualTo(SUM_SALARIES_DEP_1);
     }
 
     @Test
     void calculateSumSalariesBySecondDepartmentTest() {
-        when(employeeService.findAllEmployees()).thenReturn(employees);
-
         assertThat(out.calculateSumSalariesByDepartment(SECOND_DEP)).isEqualTo(SUM_SALARIES_DEP_2);
-    }
-
-
-    @Test
-    void calculateSumSalariesWhenDepartmentIsEmptyTest() {
-        when(employeeService.findAllEmployees()).thenReturn(Collections.emptyList());
-
-        assertThat(out.calculateSumSalariesByDepartment(FIRST_DEP)).isEqualTo(0);
     }
 
     @Test
     void getMaxSalaryByFirstDepartmentTest() {
-        when(employeeService.findAllEmployees()).thenReturn(employees);
-
         assertThat(out.getMaxSalaryByDepartment(FIRST_DEP)).isEqualTo(MAX_SALARY_DEP_1);
     }
 
     @Test
     void getMaxSalaryBySecondDepartmentTest() {
-        when(employeeService.findAllEmployees()).thenReturn(employees);
-
         assertThat(out.getMaxSalaryByDepartment(SECOND_DEP)).isEqualTo(MAX_SALARY_DEP_2);
     }
 
     @Test
     void getMinSalaryByFirstDepartmentTest() {
-        when(employeeService.findAllEmployees()).thenReturn(employees);
-
         assertThat(out.getMinSalaryByDepartment(FIRST_DEP)).isEqualTo(MIN_SALARY_DEP_1);
     }
 
     @Test
     void getMinSalaryBySecondDepartmentTest() {
-        when(employeeService.findAllEmployees()).thenReturn(employees);
-
         assertThat(out.getMinSalaryByDepartment(SECOND_DEP)).isEqualTo(MIN_SALARY_DEP_2);
     }
 
     @Test
     void getEmployeesGroupByDepartments() {
-        when(employeeService.findAllEmployees()).thenReturn(employees);
-
         Map<Integer, List<Employee>> expected = Map.of(
                 FIRST_DEP, List.of(EMPLOYEE_1_DEP_1, EMPLOYEE_2_DEP_1),
                 SECOND_DEP, List.of(EMPLOYEE_1_DEP_2, EMPLOYEE_2_DEP_2)
